61fcb4ffd6984eba892bed761bd3b28d2b772832
ChangMin <cckdals222@gmail.com>
2023-09-20 09:06:28 +0900

    feat: navbar shadow 추가

--- src/component/Nav/NavBar.tsx
+++ src/component/Nav/NavBar.tsx
@@ -1,6 +1,6 @@
 export const NavBar = () => {
   return (
-    <div className="w-full h-24 flex flex-row justify-between items-center px-5 lg:px-20">
+    <div className="w-full h-24 flex flex-row justify-between items-center px-5 lg:px-20 shadow-lg">
       <span>Menu</span>
       <span>Movie Review</span>
       <span>Login</span>
