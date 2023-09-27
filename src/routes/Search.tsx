a943f179b582149ca52175d4a6d7d8ad7ebcacee
ChangMin <cckdals222@gmail.com>
2023-09-27 12:14:47 +0900

    feat: 검색결과 없을때 보여줄 화면 생성

--- src/routes/Search.tsx
+++ src/routes/Search.tsx
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom";
+import { FcQuestions } from "react-icons/fc";
 
 import { Poster } from "@src/component/List/Poster";
 import { useFetchSearch } from "@src/hooks/useFetchSearch";
@@ -9,6 +10,27 @@ const Search = () => {
   const { data } = useFetchSearch(keyword);
 
   const list = validateSearchResult(data);
+
+  if (data?.results.length === 0) {
+    return (
+      <div className="bg-slate-100 px-5 lg:px-20">
+        <div className="flex items-center pt-5 text-xl font-semibold">
+          <span>"{keyword}"의 검색결과</span>
+        </div>
+
+        <div className="flex justify-center items-center flex-col gap-y-1 min-h-screen">
+          <div className="-translate-y-20">
+            <FcQuestions size={120} />
+          </div>
+          <div className="flex -translate-y-20">
+            <span className="font-bold">{keyword}</span>
+            <span>의 검색결과가 없습니다</span>
+          </div>
+          <div className="-translate-y-20">다른 키워드로 검색해보세요.</div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <>
