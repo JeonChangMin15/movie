581212db0b316892266c96b508eac53467588ef8
ChangMin <cckdals222@gmail.com>
2023-09-27 10:48:17 +0900

    feat: cinema inside로 변경

--- src/component/Nav/NavBar.tsx
+++ src/component/Nav/NavBar.tsx
@@ -2,7 +2,7 @@ export const NavBar = () => {
   return (
     <div className="w-full h-24 flex flex-row justify-between items-center px-5 lg:px-20 shadow-lg">
       <span>Menu</span>
-      <span>Movie Review</span>
+      <span>Cinema inside</span>
       <span>Login</span>
     </div>
   );
