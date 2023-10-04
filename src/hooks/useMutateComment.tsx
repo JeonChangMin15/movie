f4effdbf0df3d1581989eac3c374eea36f10c0b3
ChangMin <cckdals222@gmail.com>
2023-10-04 14:13:18 +0900

    chore: 주석, 불필요한 파일 제거

--- src/hooks/useMutateComment.tsx
+++ src/hooks/useMutateComment.tsx
@@ -17,16 +17,13 @@ export const useMutateComment = (movieId: string | undefined = "") => {
   const addComment = async (info: Info, movieId: string | undefined = "") => {
     const docRef = doc(db, "movie_comment", movieId);
 
-    // Check if the document exists
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
-      // If document exists, update the data
       await updateDoc(docRef, {
         data: arrayUnion(info),
       });
     } else {
-      // If document doesn't exist, create it and set the data
       await setDoc(docRef, { data: [info] });
     }
   };
